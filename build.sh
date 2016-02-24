b321bbcff921407b87a17d34b57af2b7f132b548
Silvan Gacond <silvan@gacond.ch>
2016-02-24 08:40:18 +0100

    build

--- build.sh
+++ build.sh
@@ -12,14 +12,8 @@ fi
 # work around restore timeouts on Mono
 [ -z "$MONO_THREADS_PER_CPU" ] && export MONO_THREADS_PER_CPU=50
 
-export DNX_UNSTABLE_FEED=https://www.myget.org/F/aspnetcidev/api/v2
 dnvm update-self
 
-dnvm install 1.0.0-rc2-16444 -u -r mono
+dnvm install upgrade -r mono
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
-dnvm install 1.0.0-rc2-16444 -u -r coreclr
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-
-dnu restore --quiet --parallel
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
